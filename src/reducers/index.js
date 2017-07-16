250a25891757b55131030eceb66cc5b0955f1bec
pplayer66 <lander89@inbox.ru>
2017-07-16 12:11:15 +0600

    .

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import newsReducer from './news_reducer';
-// import currentPageReducer from './pages_reducer';
 
 const rootReducer = combineReducers({
 	news: newsReducer
